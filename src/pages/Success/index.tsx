af4d5c4ff45972e3ab1ad9b2f264dece411b9b18
KevinGuedes <kevinguedes@edu.unifor.br>
2022-10-13 20:44:56 -0300

    feat(success): Success page created

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,5 +1,51 @@
 import { SuccessContainer } from './styles'
 
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
+import deliveryGuy from '../../assets/delivery-guy.png'
+import { Information } from '../../components/Information'
+
 export function Success(): JSX.Element {
-  return <SuccessContainer>index</SuccessContainer>
+  return (
+    <SuccessContainer>
+      <h1>Uhu! Pedido confirmado</h1>
+      <p>Agora é só aguardar que logo o café chegará até você</p>
+      <section>
+        <div>
+          <div>
+            <Information.Root>
+              <Information.Icon circleColor="purple">
+                <MapPin size={18} weight="fill" />
+              </Information.Icon>
+              <Information.Text>
+                Entrega em Rua João Daniel Martinelli, 102 <br />
+                Farrapos - Porto Alegre, RS
+              </Information.Text>
+            </Information.Root>
+
+            <Information.Root>
+              <Information.Icon circleColor="yellow">
+                <Timer size={18} weight="fill" />
+              </Information.Icon>
+              <Information.Text>
+                Previsão de entrega <br />
+                <strong>20 min - 30 min</strong>
+              </Information.Text>
+            </Information.Root>
+
+            <Information.Root>
+              <Information.Icon circleColor="yellow-dark">
+                <CurrencyDollar size={18} weight="fill" />
+              </Information.Icon>
+              <Information.Text>
+                Previsão de entrega <br />
+                <strong>20 min - 30 min</strong>
+              </Information.Text>
+            </Information.Root>
+          </div>
+        </div>
+
+        <img src={deliveryGuy} alt="Pessoa entregando café em uma moto" />
+      </section>
+    </SuccessContainer>
+  )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,14 +1,46 @@
 import styled from 'styled-components'
 
 export const SuccessContainer = styled.main`
-  /* box-sizing: content-box;
-  border-width: 1px;
-  border-style: solid;
-  border-image: linear-gradient(
-    to right,
-    ${props => props.theme.brand.yellow},
-    ${props => props.theme.brand.purple}
-  );
-  border-image-slice: 1;
-  border-radius: 6px 36px; */
+  margin-top: 5rem;
+
+  h1 {
+    font-family: 'Baloo 2';
+    color: ${props => props.theme.brand['yellow-dark']};
+    line-height: 1.3;
+  }
+
+  p {
+    line-height: 1.3;
+    color: ${props => props.theme.base.subtitle};
+  }
+
+  section {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 6rem;
+    margin-top: 1.75rem;
+
+    > div {
+      border: 1px solid transparent;
+      border-radius: 6px 36px;
+      width: 33rem;
+
+      background-image: linear-gradient(white, white),
+        linear-gradient(
+          to right,
+          ${props => props.theme.brand.yellow},
+          ${props => props.theme.brand.purple}
+        );
+      background-origin: border-box;
+      background-clip: content-box, border-box;
+
+      > div {
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        padding: 2.5rem;
+      }
+    }
+  }
 `
