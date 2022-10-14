b0e1043056783e3b213c9df5e9d6bd6c91a0ff36
KevinGuedes <kevinguedes@edu.unifor.br>
2022-10-14 20:56:59 -0300

    feat(payment): toggle group for payment type

--- src/pages/Purchase/components/PurcahseForm/index.tsx
+++ src/pages/Purchase/components/PurcahseForm/index.tsx
@@ -1,12 +1,27 @@
-import { MapPinLine, CurrencyDollar } from 'phosphor-react'
+import {
+  MapPinLine,
+  CurrencyDollar,
+  CreditCard,
+  Bank,
+  Money
+} from 'phosphor-react'
+import * as ToggleGroup from '@radix-ui/react-toggle-group'
+
 import {
   FormSectionHeader,
   MainDataContainer,
   PaymentTypeContainer,
   PurchaseFormContainer
 } from './styles'
+import { useState } from 'react'
 
 export function PurchaseForm(): JSX.Element {
+  const [paymentType, setPaymentType] = useState('')
+
+  function handlePaymentTypeChange(paymentType: string): void {
+    setPaymentType(paymentType)
+  }
+
   return (
     <PurchaseFormContainer>
       <h3>Complete seu pedido</h3>
@@ -31,6 +46,25 @@ export function PurchaseForm(): JSX.Element {
               </p>
             </div>
           </FormSectionHeader>
+
+          <ToggleGroup.Root
+            type="single"
+            value={paymentType}
+            onValueChange={handlePaymentTypeChange}
+          >
+            <ToggleGroup.Item value="credit-card">
+              <CreditCard size={16} />
+              Certão de crédito
+            </ToggleGroup.Item>
+            <ToggleGroup.Item value="debit-card">
+              <Bank size={16} />
+              Certão de débito
+            </ToggleGroup.Item>
+            <ToggleGroup.Item value="money">
+              <Money size={16} />
+              Dinheiro
+            </ToggleGroup.Item>
+          </ToggleGroup.Root>
         </PaymentTypeContainer>
       </form>
     </PurchaseFormContainer>
--- src/pages/Purchase/components/PurcahseForm/styles.ts
+++ src/pages/Purchase/components/PurcahseForm/styles.ts
@@ -3,6 +3,7 @@ import { defaultTheme } from '../../../../styles/themes/defaultTheme'
 
 export const PurchaseFormContainer = styled.section`
   flex: 1;
+  max-width: 40rem;
 
   h3 {
     font-family: 'Baloo 2';
@@ -33,6 +34,7 @@ export const FormSectionHeader = styled.header<FormSectionHeaderProps>`
   display: flex;
   gap: 0.5rem;
   align-items: flex-start;
+  margin-bottom: 2rem;
 
   svg {
     color: ${props => ICON_COLORS[props.iconColor]};
@@ -62,4 +64,48 @@ const BaseDataContainer = styled.div`
 
 export const MainDataContainer = styled(BaseDataContainer)``
 
-export const PaymentTypeContainer = styled(BaseDataContainer)``
+export const PaymentTypeContainer = styled(BaseDataContainer)`
+  div[role='group'] {
+    display: flex;
+    gap: 0.75rem;
+
+    button[data-state='on'] {
+      border: 1px solid ${props => props.theme.brand.purple};
+      background-color: ${props => props.theme.brand['purple-light']};
+    }
+
+    button {
+      flex: 1;
+      padding: 1rem;
+      border: 1px solid transparent;
+      border-radius: 6px;
+      font-size: 0.75rem;
+      line-height: 1.6;
+      font-weight: 400;
+      cursor: pointer;
+
+      display: flex;
+      gap: 0.5rem;
+      align-items: center;
+
+      color: ${props => props.theme.base.text};
+      background-color: ${props => props.theme.base.button};
+      transition: background-color 0.2s;
+      transition: color 0.2s;
+
+      svg {
+        color: ${props => props.theme.brand.purple};
+        transition: color 0.2s;
+      }
+
+      &:hover {
+        color: ${props => props.theme.base.subtitle};
+        background-color: ${props => props.theme.base.hover};
+
+        svg {
+          color: ${props => props.theme.brand['purple-dark']};
+        }
+      }
+    }
+  }
+`
